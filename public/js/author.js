d8a19fde5d957e94cbc4ed08586cd304458dc5fa
Benjamin Brier <ben.brier14@gmail.com>
2019-11-25 15:35:01 -0500

    changes did not work, current code is functional

--- public/js/author.js
+++ public/js/author.js
@@ -34,11 +34,14 @@ $(document).ready(function() {
   
     // Function for creating a new list row for authors
     function createAuthorRow(authorData) {
-      console.log(authorData);
       var newTr = $("<tr>");
       newTr.data("author", authorData);
       newTr.append("<td>" + authorData.name + "</td>");
-      newTr.append("<td># of posts will display when we learn joins in the next activity!</td>");
+      if (authorData.Posts) {
+        newTr.append("<td> " + authorData.Posts.length + "</td>");
+      } else {
+        newTr.append("<td>0</td>");
+      }
       newTr.append("<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>");
       newTr.append("<td><a href='/cms?author_id=" + authorData.id + "'>Create a Post</a></td>");
       newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
